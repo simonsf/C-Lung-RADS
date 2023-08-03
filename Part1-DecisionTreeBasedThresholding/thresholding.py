37ff2711c7831cf27f734418895d8a0f59ea8c07
Feng Shi <simonsf@gmail.com>
2023-08-03 18:03:27 +0800

    Add files via upload

--- Part1-DecisionTreeBasedThresholding/thresholding.py
+++ Part1-DecisionTreeBasedThresholding/thresholding.py
@@ -2,6 +2,7 @@ import os
 import importlib
 import sys
 import argparse
+import json
 from imp import reload
 from utils.build_decision_tree import *
 from utils.grid_search import *
@@ -40,7 +41,7 @@ class Thresholding():
         self.target = cfg.pathology_label_column
         self.level = cfg.manual_label_column
         self.mal = cfg.malignancy_prob_column
-        self.mal_thres = cfg.malignancy_prob_column
+        self.mal_thres = cfg.malignancy_prob_thres
         if self.level is not None and self.target is not None:
             self.data = sample_weight_df(self.data, self.level, self.target)
             self.sw = 'sample_weight'
@@ -51,7 +52,16 @@ class Thresholding():
         self.grid_config = cfg.grading
 
         self.thres_ranges = None
-        self.total_results = None 
+        self.total_results = None
+
+        self._define_rule_pattern()
+
+    def _define_rule_pattern(self):
+        self.solid_pattern = '    L1: < %d mm \n    L2: >= %d to < %d mm \n    L3: >= %d to < %d mm \n    L4: > %d mm'
+        self.glass_pattern = '    L1: < %d mm \n    L2: >= %d to < %d mm \n    L3: > %d mm' 
+        self.mix_pattern = '    L1: < %d mm \n    L2: >= %d to < %d mm total mean diameter with solid component < %d mm \n\
+    L3: >= %d to < %d mm total mean diameter with solid component >= %d to < %d mm \n\
+    L4: >= %d mm total mean diameter or solid component >= %d mm'
     
     def _generate_thres_ranges(self):
         self.solid_data = self.data[self.data.NoduleType==1]
@@ -87,16 +97,16 @@ class Thresholding():
         solid_candids = grid_search_solid(self.solid_data, self.thres_ranges['Solid'], 
                                           self.vars[0], self.target, min_rate=self.grid_config.min_proportion['solid'],
                                           weight=self.sw)
-        solid_candids = solid_candids['thres'].iloc[:self.grid_config.num_topK]
+        solid_candids = list(solid_candids['thres'].iloc[:self.grid_config.num_topK])
 
         glass_candids = grid_search_glass(self.glass_data, self.thres_ranges['pGGN'], 
                                           self.vars[0], self.target, min_rate=self.grid_config.min_proportion['pGGN'],
                                           weight=self.sw)
-        glass_candids = glass_candids['thres'].iloc[:self.grid_config.num_topK]
+        glass_candids = list(glass_candids['thres'].iloc[:self.grid_config.num_topK])
 
         mix_candids = grid_search_mix(self.mix_data, self.thres_ranges['mGGN']['axis'], self.thres_ranges['mGGN']['solid'], 
                                       self.vars, self.target, min_rate=self.grid_config.min_proportion['mGGN'])
-        mix_candids = mix_candids['thres'].iloc[:self.grid_config.num_topK]
+        mix_candids = list(mix_candids['thres'].iloc[:self.grid_config.num_topK])
 
         sort_key = 'iv_sum' if self.mal is None else 'iv_sum_4x'
         total_results = grid_search_all(self.data, self.vars, self.target, solid_candids, glass_candids, mix_candids, 
@@ -108,6 +118,21 @@ class Thresholding():
         self._generate_thres_ranges()
         self._grid_search_combinations()
 
+    def _parse_thres_into_rule(self, thres):
+        s = thres['Solid']
+        solid_rule = self.solid_pattern%(s[0], s[0], s[1], s[1], s[2], s[2])
+
+        s = thres['pGGN']
+        glass_rule = self.glass_pattern%(s[0], s[0], s[1], s[1])
+
+        s = thres['mGGN']
+        a1 = s['axis']
+        a2 = s['solid']
+        mix_rule = self.mix_pattern%(a1[0], a1[0], a1[1], a2[0], a1[0], a1[1], a2[0], a2[1], a1[1], a2[1])
+        
+        total_rule = 'Solid: \n%s\n pGGN: \n%s\n mGGN: \n%s\n' %(solid_rule, glass_rule, mix_rule)
+        return total_rule
+
     def get_result(self):
         assert self.total_results is not None
         top1_thres = self.total_results.thres.iloc[0]
@@ -118,7 +143,11 @@ class Thresholding():
             return top1_thres
         if not os.path.isdir(self.save_dir):
             os.makedirs(self.save_dir)
-        self.total_results.to_csv(os.path.join(self.save_dir, 'total_thresholding_results.csv'), index=None)
+        self.total_results.to_csv(os.path.join(self.save_dir, 'topK_thresholds.csv'), index=None)
+        best_rule = self._parse_thres_into_rule(top1_thres)
+        print('Best: \n', best_rule)
+        with open(os.path.join(self.save_dir, 'thresholding.txt'), 'w') as f:
+            f.write(best_rule)
         return top1_thres
 
 
@@ -126,7 +155,6 @@ def run_thres_pipeline(config_file):
     th = Thresholding(config_file)
     th.run()
     res = th.get_result()
-    print('best threshold: ', res)
     return res
 
 
